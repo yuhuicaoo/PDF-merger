c7e8d7a80e8481750a4d1b33ee71c3b970a7dbc6
Yuhui Cao <ycao891@aucklanduni.ac.nz>
2024-06-30 00:57:18 +1200

    Initial commit

---
+++ main.py
@@ -0,0 +1,39 @@
+from PyPDF2 import PdfMerger
+import os
+import tkinter as tk
+from tkinter import filedialog, messagebox
+
+os.environ["TK_SILENCE_DEPRECATION"] = "1"
+
+
+def merge_pdfs(folder_path):
+    merger = PdfMerger()
+
+    # Assuming all files in the folder_path are PDFs
+    pdf_files = [
+        f for f in os.listdir(folder_path) if f.endswith(".pdf") and f != "merged.pdf"
+    ]
+    pdf_files.sort()
+
+    for filename in pdf_files:
+        merger.append(os.path.join(folder_path, filename))
+
+    output_filename = os.path.join(folder_path, "merged.pdf")
+    merger.write(output_filename)
+    merger.close()
+
+    messagebox.showinfo(
+        "Merge Complete", f"PDFs merged successfully into {output_filename}"
+    )
+
+
+def main():
+    root = tk.Tk()
+    root.withdraw()
+    folder_path = filedialog.askdirectory()
+    if folder_path:
+        merge_pdfs(folder_path)
+
+
+if __name__ == "__main__":
+    main()
